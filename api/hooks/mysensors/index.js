2306f74664a988e83f34886ab79da2ae78880364
Nicky Eichmann <enicky80@gmail.com>
2015-11-04 14:17:22 +0100

    Added serialport functionality

--- api/hooks/mysensors/index.js
+++ api/hooks/mysensors/index.js
@@ -1,12 +1,20 @@
 /**
  * Created by enicky on 4/11/2015.
  */
+var SerialPort = require("serialport").SerialPort;
+
 
 module.exports = function mySimpleHook(sails) {
 
   return {
     startMySensorsProcess: function(){
       sails.log('debug','Starting the Process to poll for the serial port ... ');
+      var serialPort = new SerialPort("/dev/ttyMySensorsGateway");
+      serialPort.on('data', function(data){
+        var d = data.toString('utf8');
+        d = d.replace(/(\r\n|\n|\r)/gm,"");
+        console.log('data : ', d);
+      })
     },
 
     // Runs automatically when the hook initializes
