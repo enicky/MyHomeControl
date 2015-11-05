1d3acb870e9549e98ec124aa44e647edbaa2f0e8
Nicky Eichmann <enicky80@gmail.com>
2015-11-05 22:00:58 +0100

    Added serialport functionality

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -7,20 +7,8 @@
 
 module.exports = {
 	index : function(req, res){
-    var someAttributes = {
-      lastName: 'Simpson',
-      favoriteFood: 'beer'
-    };
 
-    var id = 'homer@simpsons.com';
-
-
-      MyModel.find({limit:5}).sort({ createdAt: 'desc' }).exec(function(err, mymodels){
-        mymodels.forEach(function(index, item){
-          sails.log('debug','Item => ', item, index.name, index.createdAt);
-        });
         return res.view('homepage');
-      })
 
 
 
